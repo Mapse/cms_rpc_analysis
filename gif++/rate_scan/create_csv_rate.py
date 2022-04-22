5695f7c8b23a0f8301fc855fef4154ab6c448707
Mapse <mapse.b@hotmail.com>
2022-04-22 01:41:17 -0300

    Update with comments

--- gif++/rate_scan/create_csv_rate.py
+++ gif++/rate_scan/create_csv_rate.py
@@ -1,19 +1,22 @@
 import json, os, re, pprint
 import pandas as pd
 import getpass
-import config as cg
+import config_csv_rate as cg
 
+# Take the chamber name and Hv scan path from config file.
 chamber = cg.chamber 
 hvscan_path = cg.hvscan_path 
 
 
-
+# Function to sort the HV points in ascending order
 def natural_sort(l): 
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key=alphanum_key)   
 
+# Function that reads json file and insert its data on a csv file by using pandas
 def output_csv(run_number):
+    # Path to the json file. Each scan should have one json file.
     file_path =  f"{hvscan_path}/{run_number:06}/ANALYSIS/{chamber}"
     with open(file_path + '/output.json') as json_file:
         output = json.load(json_file)
@@ -29,6 +32,7 @@ def output_csv(run_number):
         df.to_csv(f'data_{chamber}_{run_number:06}.csv', index=False)
 
 if __name__ == "__main__":
+    # Take the scan list and produce all csv files of interest,
     run_number_list = cg.run_number_list
 
     for run_number in run_number_list:
