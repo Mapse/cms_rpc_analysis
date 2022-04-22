680e26336500d29a33d9ce4001603327090d477c
Mapse <mapse.b@hotmail.com>
2022-04-22 00:56:21 -0300

    Adding rate scan codes

---
+++ gif++/rate_scan/config.py
@@ -0,0 +1,10 @@
+## For csv script
+
+# Chamber name in rate scan folder (see webdcs)
+chamber = "KODELC_RATE"
+# Path to scans
+hvscan_path = "/var/webdcs/HVSCAN"
+# List with runs to produce csv
+run_number_list = [5092, 5093, 5094, 5095]
+
+
