7131f8efe16953389cc353baa514bb5e259bb1ab
Mapse <mapse.b@hotmail.com>
2022-06-01 01:43:35 -0300

    Config added.

---
+++ gif++/test_beam/get_data/config.py
@@ -0,0 +1,26 @@
+## For csv script
+
+# hvscan path
+hvscan_path = "/var/webdcs/HVSCAN"
+# path chamber
+path_chamb = '/test_beam_april_2022/KodelC'
+
+## For produce csv and move to the folder
+
+## Runs
+
+# 300 uV - 45 fC runs
+# 2.2, 3.3, 4.6, 6.9, 10, 22, 69, 100, OFF
+
+# 300 uV - 45 fC runs
+# 2.2, 3.3, 4.6, 6.9, 10, 22, 69, 100, 220, OFF
+
+
+""" runs_300 = ["5115", "5118", "5117", "5116", "5114",
+        "5119", "5120", "5121", "5112"] """
+
+runs = ["5129", "5135", "5133", "5132", "5130",
+        "5127", "5126", "5145", "5125", "5124"]
+# Chamber
+chamber = "KODELC"
+# type of the code
