290350192d7217ef56af0c4264da9850607123f9
Mapse <mapse.b@hotmail.com>
2022-08-16 10:38:25 -0300

    update config with RE1_1

--- gif++/test_beam/get_data/config.py
+++ gif++/test_beam/get_data/config.py
@@ -3,24 +3,28 @@
 # hvscan path
 hvscan_path = "/var/webdcs/HVSCAN"
 # path chamber
-path_chamb = '/test_beam_april_2022/KodelC'
+path_chamb = "/RE1_1/tb_july_2022"
 
-## For produce csv and move to the folder
+## To produce csv and move to the folder
 
 ## Runs
 
-# 300 uV - 45 fC runs
-# 2.2, 3.3, 4.6, 6.9, 10, 22, 69, 100, OFF
-
-# 300 uV - 45 fC runs
-# 2.2, 3.3, 4.6, 6.9, 10, 22, 69, 100, 220, OFF
-
+# Std gas
+#308: 3.3
+#306: 4.6 (no)
+#305: 6.9
+#304: 10 (bad)
+#303: 22 (no)
+#301: 46 
+#300: 69
+#299: 100
+#294: OFF
 
 """ runs_300 = ["5115", "5118", "5117", "5116", "5114",
         "5119", "5120", "5121", "5112"] """
 
-runs = ["5129", "5135", "5133", "5132", "5130",
-        "5127", "5126", "5145", "5125", "5124"]
+runs = ["000308", "000305", "000301", "000300", "000299", "000294",]
 # Chamber
-chamber = "KODELC"
+chamber = "RE1_B"
 # type of the code
+#Footer
