dea196d90e1c7c715fe2e8813f5ecb14977cf5fc
Mapse <mapse.b@hotmail.com>
2022-08-16 10:40:44 -0300

    Version with config file added

--- gif++/test_beam/get_data/create_csv_WP.py
+++ gif++/test_beam/get_data/create_csv_WP.py
@@ -2,17 +2,19 @@ import json, os, re, pprint
 import pandas as pd
 import getpass
 
+import config
+
 def natural_sort(l): 
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key=alphanum_key)    
 
 def output_csv(run_number, chamber):
-    hvscan_path = "/var/webdcs/HVSCAN"
-    file_path = f"{hvscan_path}/{run_number:06}/ANALYSIS/{chamber}" 
+    hvscan_path = config.hvscan_path
+    file_path = hvscan_path + '/' + run_number + '/ANALYSIS/' + chamber
     #file_path = f"{chamber}"
     user = getpass.getuser()
-    save_path = f'/var/webdcs/ANALYSIS/{user}/test_beam_october_2021/RE1_1'
+    save_path = f'/var/webdcs/ANALYSIS/{user}' + config.path_chamb
     
     with open(file_path + '/output.json') as json_file:
         output = json.load(json_file)
@@ -21,17 +23,12 @@ def output_csv(run_number, chamber):
         data.append([output[column] for column in columns])
         df = pd.DataFrame(data, columns=columns)
         print(df.keys())
-        df.to_csv(f'{save_path}/data_{chamber}_{run_number:06}.csv', index=False)
+        df.to_csv(f'{save_path}/data_{chamber}_{run_number}_WP.csv', index=False)
 
 if __name__ == "__main__":
-    import argparse
 
-    parser = argparse.ArgumentParser(description='Output csv data')
-    parser.add_argument('run_number', metavar='run_number', type=int, nargs=1, help='run number')
-    parser.add_argument('chamber', metavar='chamber', type=str, nargs=1, help='chamber name')
-
-    args = parser.parse_args()
-    run_number = args.run_number[0]
-    chamber = args.chamber[0]
+    runs = config.runs
+    chamber = config.chamber
+    for i in runs:
     
-    output_csv(run_number, chamber)
+        output_csv(i, chamber)
