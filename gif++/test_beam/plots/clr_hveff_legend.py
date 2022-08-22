61b5bbbd37a0b94c1000be6e50871a4ef28b1ba0
Mapse <mapse.b@hotmail.com>
2022-08-22 20:24:40 -0300

    Name changed and kV inserted

--- gif++/test_beam/plots/clr_hveff_legend.py
+++ gif++/test_beam/plots/clr_hveff_legend.py
@@ -3,7 +3,7 @@ import pandas as pd
 def compute_cluster_rate_WP(path, file):
     
     # List to pass the gamma cluster size for each csv
-    gamma_cs_list = []
+    gamma_cs_wp_list = []
         
     for f in file:
         # Read the csv file as a panda dataframe
@@ -15,7 +15,9 @@ def compute_cluster_rate_WP(path, file):
         gamma_hit_rate = df['noiseGammaRate_WP_muon'][0]
         # Compute the cluster rate
         cluster_rate = gamma_hit_rate / gamma_cs
-        #gamma_cs_list.append(f'Clus. Rat.:{round(2, cluster_rate)}')
-        gamma_cs_list.append(f'Clus. Rate: {round(float(cluster_rate), 2)} $Hz/cm^2$')
+        wp_muon_corr = df['WP_muon_corr'][0]/1000
+        
+        #gamma_cs_wp_list.append(f'{round(float(cluster_rate), 2)} $Hz/cm^2$, {wp_muon_corr} V')
+        gamma_cs_wp_list.append(f'CLR: {float(cluster_rate):.2f} $Hz/cm^2$ | WP: {wp_muon_corr:.2f} kV')
                 
-    return gamma_cs_list
+    return gamma_cs_wp_list
