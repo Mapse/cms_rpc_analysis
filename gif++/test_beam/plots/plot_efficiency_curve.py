e25ee649d9550a5244537b57ba2af6c84a631694
Mapse <mapse.b@hotmail.com>
2022-10-18 01:11:37 -0300

    plot label added

--- gif++/test_beam/plots/plot_efficiency_curve.py
+++ gif++/test_beam/plots/plot_efficiency_curve.py
@@ -23,7 +23,7 @@ plot_eff(file, legend)
 def sigmoid(x, a, b, c):
     return a/(1 + np.exp(-b*(x-c)))
 
-def plot_eff(path, file, legend, gas, fit=False, typ=None, p0=[96.6, 0.007, 12.0]):
+def plot_eff(path, file, legend, chamber, gaps, plot, gas=None, fit=False, typ=None, p0=[96.6, 0.007, 12.0]):
     
     # List to pass the efficiency for each csv
     eff_list = []
@@ -35,7 +35,10 @@ def plot_eff(path, file, legend, gas, fit=False, typ=None, p0=[96.6, 0.007, 12.0
         # Read the csv file as a panda dataframe
         df = pd.read_csv(path + f)
         # Takes the effective voltage
-        voltage = df['hveff_' + 'RE1_1_001-BOT'].astype(float)
+        try:
+            eff_volt = df['hveff_' + chamber + '-' + list(gaps.keys())[0]]
+        except:
+            eff_volt = df['hveff_' + chamber + '-' + list(gaps.keys())[1]]
             
         # Takes the efficiency     
         efficiency = df['efficiencyMuon_corrected']
@@ -43,7 +46,7 @@ def plot_eff(path, file, legend, gas, fit=False, typ=None, p0=[96.6, 0.007, 12.0
         # Insert the desired values on the lists
         eff_list.append(efficiency)
         eff_error_list.append(efficiency_error)
-        voltage_list.append(voltage/1000)
+        voltage_list.append(eff_volt/1000)
         
     # Figure and axis
     fig, ax = plt.subplots()
@@ -69,7 +72,10 @@ def plot_eff(path, file, legend, gas, fit=False, typ=None, p0=[96.6, 0.007, 12.0
             from scipy import optimize
         
             params, params_covariance = optimize.curve_fit(sigmoid, vo, ef, p0=p0)
-            plt.plot(vo, sigmoid(vo, params[0], params[1], params[2]), color=col)
+            v_min = vo.iloc[0]
+            v_max = vo.iloc[-1]
+            x = np.linspace(v_min, v_max, 200)
+            plt.plot(x, sigmoid(x, params[0], params[1], params[2]), color=col)
 
     # Xlabel
     ax.xaxis.set_label_coords(0.5, -0.055)
@@ -82,13 +88,19 @@ def plot_eff(path, file, legend, gas, fit=False, typ=None, p0=[96.6, 0.007, 12.0
     #plt.legend('labels')
 
     # CMS format
-    hfont = {'fontname':'Helvetica'}    
-    plt.text(0.12, 0.89, "Ecogas@GIF++:", fontdict=hfont,  fontweight='bold', fontsize=14, transform=plt.gcf().transFigure) # Value for on top: 0.17, 0.89, inside plot: 0.17, 0.80
-    plt.text(0.31, 0.89, "(ALICE, ATLAS, CMS, EPDT, LHCb/SHiP)", fontdict=hfont, style='italic',fontsize = 15, transform=plt.gcf().transFigure) # Value for on top: 0.27, 0.89, inside plot: 0.27, 0.80
+    if plot == 'rpc':
+        hfont = {'fontname':'Helvetica'}
+        plt.text(0.13, 0.89, "CMS MUON", fontdict=hfont,  fontweight='bold', transform=plt.gcf().transFigure) # Value for on top: 0.17, 0.89, inside plot: 0.17, 0.80
+        plt.text(0.37, 0.89, "Preliminary", fontdict=hfont, style='italic',fontsize = 23, transform=plt.gcf().transFigure) # Value for on top: 0.27, 0.89, inside plot: 0.27, 0.80
+        plt.text(0.77, 0.89, "GIF++", fontdict=hfont,  fontweight='bold', transform=plt.gcf().transFigure) # Value for on top: 0.17, 0.89, inside plot: 0.17, 0.80
+    elif plot == 'ecogas':
+        hfont = {'fontname':'Helvetica'}    
+        plt.text(0.13, 0.89, "Ecogas@GIF++:", fontdict=hfont,  fontweight='bold', fontsize=15, transform=plt.gcf().transFigure) # Value for on top: 0.17, 0.89, inside plot: 0.17, 0.80
+        plt.text(0.32, 0.89, "(ALICE, ATLAS, CMS, EPDT, LHCb/SHiP)", fontdict=hfont, style='italic',fontsize = 15, transform=plt.gcf().transFigure) # Value for on top: 0.27, 0.89, inside plot: 0.27, 0.80
+        
+        plt.axhline(y=100, color='black', linestyle='dotted')
     
-    # Gas type
-    if gas != None:
-        plt.text(0.14, 0.85, f"{gas}", fontdict=hfont, style='italic',fontsize = 14, transform=plt.gcf().transFigure) # Value for on top: 0.27, 0.89, inside plot: 0.27, 0.80
+   
 
     ### If ones would like to move the scientific notation
     #t = ax.yaxis.get_offset_text()
